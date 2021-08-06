de38186616d0b5913fc403cb486d519b4b536569
crkaide <crkaide@ilstu.edu>
2021-08-06 08:45:31 -0500

    add'l analysis files

--- sql_query_join_tables.sql
+++ sql_query_join_tables.sql
@@ -10,3 +10,17 @@ ALTER COLUMN "Area of fishing" TYPE VARCHAR;
 SELECT * FROM aws_keys
 LEFT OUTER JOIN aws_values
 ON aws_keys.index = aws_values.index;
+
+SELECT * FROM aws_keys.columns;
+SELECT * FROM aws_values;
+
+-- Column names
+SELECT *
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = N'aws_keys';
+
+SELECT *
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = N'aws_values';
+
+
