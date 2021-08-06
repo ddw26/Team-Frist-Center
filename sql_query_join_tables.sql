7188593d70201fbc118240576d9e82be4ba9698d
crkaide <crkaide@ilstu.edu>
2021-08-06 09:59:15 -0500

    add joined table from sql

--- sql_query_join_tables.sql
+++ sql_query_join_tables.sql
@@ -2,16 +2,24 @@
 SELECT * FROM aws_keys;
 SELECT * FROM aws_values;
 
+-- Clear tables
+DELETE FROM aws_keys;
+DELETE FROM aws_values;
+
 -- Change datatypes for imports
 ALTER TABLE aws_values
 ALTER COLUMN "Area of fishing" TYPE VARCHAR;
 
--- Perform join
-SELECT * FROM aws_keys
+-- Perform join and save results
+SELECT * INTO aws_comp
+FROM aws_keys
 LEFT OUTER JOIN aws_values
-ON aws_keys.index = aws_values.index;
+ON aws_keys.index_k = aws_values.index_v;
 
-SELECT * FROM aws_keys.columns;
+-- Check results of join
+SELECT * FROM aws_comp;
+
+SELECT * FROM aws_keys;
 SELECT * FROM aws_values;
 
 -- Column names
